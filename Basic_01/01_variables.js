be3073dcc163d6dd93baf8aaaf973478aaa4099c
sj963samyak <100397887+sj963samyak@users.noreply.github.com>
2024-01-17 17:48:03 +0000

    container config done

---
+++ Basic_01/01_variables.js
@@ -0,0 +1,5 @@
+const accountId=20
+let accountEmail="hitesh@google.com"
+let accountCity="hyderabad"
+let accountHolderName="samyak"
+console.log([accountId,accountCity,accountEmail,accountHolderName])
