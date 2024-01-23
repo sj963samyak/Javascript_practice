611bccdc417827802ba9d8c4435f6dab4e068013
sj963samyak <100397887+sj963samyak@users.noreply.github.com>
2024-01-23 17:28:34 +0000

    container config done

--- Basic_01/loopspractice.js
+++ Basic_01/loopspractice.js
@@ -1,4 +1,4 @@
-//29,30,31,32
+//30,31,32
 //for of loop
 // const arr=[1,2,3,4]
 // for(const num of arr){
@@ -41,3 +41,44 @@ coding.forEach((item)=>{
     console.log(item + "-")
 })
 
+//filter map and reduce
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+const newNums=myNums.filter((num)=> num>4)
+console.log(newNums)
+const newNums1=myNums.filter((num)=>{
+    return num>5
+})
+console.log(newNums1)
+//when we start a scope then we will always use the return keyword
+
+//when we filter the values of object we use filter method
+const studentUsers=[
+    {
+        studentId:1,
+        studentCourse:"python"
+    },
+    {
+        studentId:2,
+        studentCourse:"java"
+    }
+]
+//find out the course of student whose id is 2
+let userCourse = studentUsers.filter((st)=>st.studentId===2)
+console.log(userCourse[0].studentCourse)
+//using map we can add the value to each array element or we can perform operation to each element
+const myNumbers=[1,2,3,4,5,6,7,8,9,10]
+const newNums3=myNumbers.map((num)=>{
+    return num+10
+})
+console.log(newNums3)
+//reduce method important
+const array1=[1,2,3,4]
+const myTotal=array1.reduce(function(acc,currValue){
+    console.log(`acc: ${acc} and current value is ${currValue}`)
+    return acc+currValue
+},0)
+
+const myTotalAr=array1.reduce((acc,currValue)=>{
+    return acc+currValue
+},0)
+console.log(myTotalAr +"--")
